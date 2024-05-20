f94230321447c97943b1ae2329faf85d8aa1d43a
wresssii <148309181+wresssii@users.noreply.github.com>
2024-05-20 15:48:18 +0600

    start

---
+++ src/firebase.js
@@ -0,0 +1,72 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, onSnapshot } from "firebase/firestore";
+import { GoogleAuthProvider, getAuth, onAuthStateChanged, signInWithPopup, signOut } from 'firebase/auth';
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAnDlxqoXQ7kNHBXVVdX0CwTyN4VfoOcmc",
+  authDomain: "techno-4a7d2.firebaseapp.com",
+  projectId: "techno-4a7d2",
+  storageBucket: "techno-4a7d2.appspot.com",
+  messagingSenderId: "708380711423",
+  appId: "1:708380711423:web:6acc2d0aeb6335de10afca"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+const auth = getAuth(app);
+export const storage = getStorage(app);
+
+export const categoryCollection = collection(db, "categories");
+export const productsCollection = collection(db, "products");
+export const ordersCollection = collection(db, "orders");
+
+const provider = new GoogleAuthProvider();
+export const LogIn = () => signInWithPopup(auth, provider);
+export const LogOut = () => signOut(auth);
+export const onAuthChange = (callback) => onAuthStateChanged(auth, callback);
+
+export const onCategoriesLoad = (callback) =>
+  onSnapshot(categoryCollection, (snapshot) =>
+    callback(
+      snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }))
+    )
+  );
+export const onProductsLoad = (callback) =>
+  onSnapshot(productsCollection, (snapshot) =>
+    callback(
+      snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }))
+    )
+  );
+export const onOrdersLoad = (callback) =>
+  onSnapshot(ordersCollection, (snapshot) =>
+    callback(
+      snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }))
+    )
+  );
+
+  // отправка фотографии и получение ее url
+export const uploadProductPhoto = (file) => {
+  const storageRef = ref(storage, `products/${file.name}`);
+  return uploadBytes(storageRef, file)
+    .then(() => {
+      return getDownloadURL(storageRef);
+    })
+    .catch((error) => {
+      console.log("Failed to upload product photo:", error);
+    });
+};
