c8965cb5956f39b6847dd326bbaeeabc7cd094b1
wresssii <148309181+wresssii@users.noreply.github.com>
2024-05-22 13:25:13 +0600

    added delivery

--- src/Pages/Delivery/Delivery.js
+++ src/Pages/Delivery/Delivery.js
@@ -1,9 +1,18 @@
 import './Delivery.css';
+import delivery from '../../img/delivery.webp'
 
 function Delivery() {
   return (
     <section className="Delivery">
-      Delivery
+      <div className="delivery_pages">
+      <img src={delivery} alt="delivery" className="delivery" />
+      <div className="delivery_text">
+      <h2 className="delivery_tit">Delivery</h2>
+      <p className="delivery_txt">Online store "Techno Mir" delivers any goods free of charge within the city of Bishkek, by its own Delivery Service. Delivery to suburban areas and distant regions of Kyrgyzstan is carried out at the rates of long-distance deliveries, at the time of placing the order.</p>
+      <p className="delivery_txt">And you can also use the services of "urgent courier delivery in the city of Bishkek" from our store from 500 soms, provided that it is delivered by our courier.</p>
+      <p className="delivery_txt">Delivery time does not exceed more than 2 days from the moment of order confirmation through the sales manager.</p>
+      </div>
+      </div>
     </section>
   );
 }
