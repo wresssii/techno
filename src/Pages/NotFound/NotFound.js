2ec3a82d2e43c8ab567736a0e1ea717c94c78620
wresssii <148309181+wresssii@users.noreply.github.com>
2024-05-22 12:43:16 +0600

    added pages 404

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,10 +1,11 @@
 import { Link } from 'react-router-dom';
 import './NotFound.css';
+import notfound from '../../img/404.webp'
 
 function NotFound() {
   return (
     <div className="container">
-      <div className='NotFound'>404</div>
+      <div className='NotFound'><img src={notfound} alt="notfound" className="notfound" /></div>
       <p className="NotFound_txt">
         Oops, go to 
         <Link to="/">Home</Link>
